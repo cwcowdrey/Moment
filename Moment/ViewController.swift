7d8b6db5a4f62994d92f238367ceba44d0ca26e7
Christopher Cowdrey <cwcowdrey@gmail.com>
2021-05-16 18:17:31 -0400

    Initial Commit

---
+++ Moment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moment
+//
+//  Created by Christopher Cowdrey on 5/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
