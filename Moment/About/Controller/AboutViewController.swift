b28631e67c8e98327b54d315f8512cc3e433a65b
Christopher Cowdrey <cwcowdrey@gmail.com>
2021-05-23 16:54:27 -0400

    format about page

--- Moment/About/Controller/AboutViewController.swift
+++ Moment/About/Controller/AboutViewController.swift
@@ -9,8 +9,25 @@ import Foundation
 import UIKit
 
 class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var imageStackView: UIStackView!
+    @IBOutlet weak var taglineLabel: UILabel!
+    
+    
         
     override func viewDidLoad() {
+        setText()
+    }
+    
+    func setText() {
+        headerLabel.text = "What is Moment?"
+        
+        descriptionLabel.text = "Moment believes that true power comes from within and you don't need any prerecorded instructions on how to do it. All of our meditations give you the tools you need to go out on your own and explore. Enjoy!"
+        
+        taglineLabel.text = "Live in the Moment!"
         
     }
+    
 }
