2adf0981b48ebd579104d364e15d1cf081c21db9
Christopher Cowdrey <cwcowdrey@gmail.com>
2021-05-30 15:09:47 -0400

    styled cells

--- Moment/Meditations/Controller/MeditationsViewController.swift
+++ Moment/Meditations/Controller/MeditationsViewController.swift
@@ -10,13 +10,18 @@ import UIKit
 
 class MeditationsViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
-    var meditations = [[Meditation(title: "B 1", type: .breathing, instructions: "do it", description: "breath ok just do it"), Meditation(title: "B 2", type: .breathing, instructions: "do it", description: "breath ok just do it"), Meditation(title: "B 3", type: .breathing, instructions: "do it", description: "breath ok just do it"), Meditation(title: "B 4", type: .breathing, instructions: "do it", description: "breath ok just do it")], [Meditation(title: "M 1", type: .mantra, instructions: "do it", description: "breath ok just do it"), Meditation(title: "M 2", type: .mantra, instructions: "do it", description: "breath ok just do it"), Meditation(title: "M 3", type: .mantra, instructions: "do it", description: "breath ok just do it"), Meditation(title: "M 4", type: .mantra, instructions: "do it", description: "breath ok just do it")]]
+    var meditations = [[Meditation(title: "B 1", type: .breathing, instructions: "do it", description: "breath ok just do it"), Meditation(title: "B 2", type: .breathing, instructions: "do it", description: "breath ok just do it"), Meditation(title: "B 3", type: .breathing, instructions: "do it", description: "breath ok just do it"), Meditation(title: "B 4", type: .breathing, instructions: "do it", description: "breath ok just do it")], [Meditation(title: "M 1", type: .mantra, instructions: "do it", description: "breath ok just do it"), Meditation(title: "M 2", type: .mantra, instructions: "do it", description: "breath ok just do it"), Meditation(title: "M 3", type: .mantra, instructions: "do it", description: "breath ok just do it"), Meditation(title: "M 4", type: .mantra, instructions: "do it", description: "breath ok just do it")], [Meditation(title: "N 1", type: .nirvana, instructions: "do it", description: "breath ok just do it"), Meditation(title: "N 2", type: .nirvana, instructions: "do it", description: "breath ok just do it"), Meditation(title: "N 3", type: .nirvana, instructions: "do it", description: "breath ok just do it"), Meditation(title: "N 4", type: .nirvana, instructions: "do it", description: "breath ok just do it")]]
+    
+    
+    private let spacing:CGFloat = 16.0
     
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         collectionView.delegate = self
         collectionView.dataSource = self
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
     
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
@@ -54,5 +59,22 @@ class MeditationsViewController: UIViewController, UICollectionViewDelegate, UIC
         cell.setupCell(with: meditations[indexPath.section][indexPath.row])
         return cell
         
+    }
+}
+
+
+extension MeditationsViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let numberOfItemsPerRow:CGFloat = 3
+        let spacingBetweenCells:CGFloat = 16
+        
+        let totalSpacing = (2 * self.spacing) + ((numberOfItemsPerRow - 1) * spacingBetweenCells) //Amount of total spacing in a row
+        
+        if let collection = self.collectionView{
+            let width = (collection.bounds.width - totalSpacing)/numberOfItemsPerRow
+            return CGSize(width: width, height: width)
+        }else{
+            return CGSize(width: 0, height: 0)
+        }
     }
 }
