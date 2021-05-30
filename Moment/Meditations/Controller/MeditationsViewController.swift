52d3892c6f851bcbb2ff4fe2892b3643996e282e
Christopher Cowdrey <cwcowdrey@gmail.com>
2021-05-30 13:26:59 -0400

    add header

--- Moment/Meditations/Controller/MeditationsViewController.swift
+++ Moment/Meditations/Controller/MeditationsViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class MeditationsViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
-    var meditations = [Meditation(title: "title", type: .breathing, instructions: "do it", description: "breath ok just do it"), Meditation(title: "title 2", type: .breathing, instructions: "do it", description: "breath ok just do it"), Meditation(title: "title 3", type: .breathing, instructions: "do it", description: "breath ok just do it"), Meditation(title: "title 4", type: .breathing, instructions: "do it", description: "breath ok just do it")]
+    var meditations = [[Meditation(title: "B 1", type: .breathing, instructions: "do it", description: "breath ok just do it"), Meditation(title: "B 2", type: .breathing, instructions: "do it", description: "breath ok just do it"), Meditation(title: "B 3", type: .breathing, instructions: "do it", description: "breath ok just do it"), Meditation(title: "B 4", type: .breathing, instructions: "do it", description: "breath ok just do it")], [Meditation(title: "M 1", type: .mantra, instructions: "do it", description: "breath ok just do it"), Meditation(title: "M 2", type: .mantra, instructions: "do it", description: "breath ok just do it"), Meditation(title: "M 3", type: .mantra, instructions: "do it", description: "breath ok just do it"), Meditation(title: "M 4", type: .mantra, instructions: "do it", description: "breath ok just do it")]]
     
     @IBOutlet weak var collectionView: UICollectionView!
     
@@ -19,10 +19,30 @@ class MeditationsViewController: UIViewController, UICollectionViewDelegate, UIC
         collectionView.dataSource = self
     }
     
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            guard let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "MeditationCollectionViewHeader", for: indexPath) as? MeditationCollectionViewHeader else {
+                fatalError()
+            }
+            
+            let headerText = meditations[indexPath.section][indexPath.row].type.rawValue
+            headerView.headerLabel.text = headerText
+            return headerView
+            
+        default:
+            assert(false, "invalid element")
+        }
+    }
+    
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return meditations[section].count
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
         return meditations.count
-        
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -31,7 +51,7 @@ class MeditationsViewController: UIViewController, UICollectionViewDelegate, UIC
             return UICollectionViewCell()
         }
         
-        cell.setupCell(with: meditations[indexPath.row])
+        cell.setupCell(with: meditations[indexPath.section][indexPath.row])
         return cell
         
     }
---
+++ Moment/Meditations/View/MeditationCollectionViewHeader.swift
@@ -0,0 +1,16 @@
+//
+//  MeditationCollectionViewHeader.swift
+//  Moment
+//
+//  Created by Christopher Cowdrey on 5/30/21.
+//
+
+import Foundation
+import UIKit
+
+class MeditationCollectionViewHeader: UICollectionReusableView {
+    
+    @IBOutlet weak var headerLabel: UILabel!
+    
+    
+}
