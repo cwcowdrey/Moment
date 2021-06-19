d5dd237092d3f5dd5aa834ff7844e3fa64aec2a4
Christopher Cowdrey <cwcowdrey@gmail.com>
2021-06-19 09:05:13 -0400

    add instructions

--- Moment/Meditations/Controller/MeditationDetailsViewController.swift
+++ Moment/Meditations/Controller/MeditationDetailsViewController.swift
@@ -12,6 +12,9 @@ class MeditationDetailsViewController: UIViewController {
     
     var meditation = Meditation()
     
+    @IBOutlet weak var mainStackView: UIStackView!
+    
+    
     @IBOutlet weak var bannerImage: UIImageView!
     
     @IBOutlet weak var descriptionLabel: UILabel!
@@ -26,6 +29,13 @@ class MeditationDetailsViewController: UIViewController {
     @IBOutlet weak var materialsValueLabel: UILabel!
     
     
+    @IBOutlet weak var instructionsLabel: UILabel!
+    
+    @IBOutlet weak var instructionsHeader: UILabel!
+    
+    @IBOutlet weak var infoStackView: UIStackView!
+    
+    
     
         
     override func viewDidLoad() {
@@ -33,6 +43,17 @@ class MeditationDetailsViewController: UIViewController {
                 
         setupNavBar()
         setupMeditationInfo()
+        
+        mainStackView.setCustomSpacing(5, after: infoStackView)
+        mainStackView.setCustomSpacing(10, after: instructionsHeader)
+        
+        infoStackView.layer.borderWidth = 2
+        infoStackView.layer.borderColor = UIColor.lightGray.cgColor
+        
+        bannerImage.layer.borderWidth = 2
+        bannerImage.layer.borderColor = UIColor.lightGray.cgColor
+        
+//        infoStackView.addBottomBorderWithColor(color: .lightGray, width: 2)
     }
     
     func setupNavBar() {
@@ -46,6 +67,39 @@ class MeditationDetailsViewController: UIViewController {
         materialsValueLabel.text = meditation.materials
         
         descriptionLabel.text = meditation.description
+        
+        instructionsLabel.text = meditation.instructions
     }
     
+}
+
+
+extension UIView {
+    func addTopBorderWithColor(color: UIColor, width: CGFloat) {
+        let border = CALayer()
+        border.backgroundColor = color.cgColor
+        border.frame = CGRect(x: 0, y: 0, width: self.frame.size.width, height: width)
+        self.layer.addSublayer(border)
+    }
+
+    func addRightBorderWithColor(color: UIColor, width: CGFloat) {
+        let border = CALayer()
+        border.backgroundColor = color.cgColor
+        border.frame = CGRect(x: self.frame.size.width - width, y: 0, width: width, height: self.frame.size.height)
+        self.layer.addSublayer(border)
+    }
+
+    func addBottomBorderWithColor(color: UIColor, width: CGFloat) {
+        let border = CALayer()
+        border.backgroundColor = color.cgColor
+        border.frame = CGRect(x: 0, y: self.frame.size.height - width, width: self.frame.size.width, height: width)
+        self.layer.addSublayer(border)
+    }
+
+    func addLeftBorderWithColor(color: UIColor, width: CGFloat) {
+        let border = CALayer()
+        border.backgroundColor = color.cgColor
+        border.frame = CGRect(x: 0, y: 0, width: width, height: self.frame.size.height)
+        self.layer.addSublayer(border)
+    }
 }
