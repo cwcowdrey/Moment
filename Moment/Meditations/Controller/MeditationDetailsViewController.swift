92ce043087f880038a5c9bec75a1368bf5ad19f4
Christopher Cowdrey <cwcowdrey@gmail.com>
2021-06-20 16:31:08 -0400

    clean up functions

--- Moment/Meditations/Controller/MeditationDetailsViewController.swift
+++ Moment/Meditations/Controller/MeditationDetailsViewController.swift
@@ -13,58 +13,44 @@ class MeditationDetailsViewController: UIViewController {
     var meditation = Meditation()
     
     @IBOutlet weak var mainStackView: UIStackView!
-    
-    
     @IBOutlet weak var bannerImage: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
-    
-    
-    
-    
     @IBOutlet weak var descriptionLabel: UILabel!
-    
     @IBOutlet weak var timeHeaderLabel: UILabel!
     @IBOutlet weak var timeValueLabel: UILabel!
-    
     @IBOutlet weak var positionHeaderLabel: UILabel!
     @IBOutlet weak var positionValueLabel: UILabel!
-    
     @IBOutlet weak var materialsHeaderLabel: UILabel!
     @IBOutlet weak var materialsValueLabel: UILabel!
-    
-    
     @IBOutlet weak var instructionsLabel: UILabel!
-    
     @IBOutlet weak var instructionsHeader: UILabel!
-    
     @IBOutlet weak var infoStackView: UIStackView!
-    
-    
-    
         
     override func viewDidLoad() {
         super.viewDidLoad()
                 
         setupNavBar()
         setupMeditationInfo()
-        
+        styleStackViews()
+    }
+    
+    func styleStackViews() {
         mainStackView.setCustomSpacing(10, after: infoStackView)
         mainStackView.setCustomSpacing(10, after: instructionsHeader)
         
         infoStackView.addBottomBorderWithColor(color: UIColor.init(hexString: "E1E4E9"), width: 2)
-        
-        titleLabel.text = meditation.title
-        
-        self.navigationController?.navigationBar.tintColor = .white
     }
     
     func setupNavBar() {
         self.title = "Meditation Details"
         self.navigationController?.navigationBar.prefersLargeTitles = false
+        self.navigationController?.navigationBar.tintColor = .white
     }
     
     
     func setupMeditationInfo() {
+        titleLabel.text = meditation.title
+        
         timeValueLabel.text = meditation.time
         positionValueLabel.text = meditation.position
         materialsValueLabel.text = meditation.materials
@@ -78,31 +64,10 @@ class MeditationDetailsViewController: UIViewController {
 
 
 extension UIView {
-    func addTopBorderWithColor(color: UIColor, width: CGFloat) {
-        let border = CALayer()
-        border.backgroundColor = color.cgColor
-        border.frame = CGRect(x: 0, y: 0, width: self.frame.size.width, height: width)
-        self.layer.addSublayer(border)
-    }
-
-    func addRightBorderWithColor(color: UIColor, width: CGFloat) {
-        let border = CALayer()
-        border.backgroundColor = color.cgColor
-        border.frame = CGRect(x: self.frame.size.width - width, y: 0, width: width, height: self.frame.size.height)
-        self.layer.addSublayer(border)
-    }
-
     func addBottomBorderWithColor(color: UIColor, width: CGFloat) {
         let border = CALayer()
         border.backgroundColor = color.cgColor
         border.frame = CGRect(x: 0, y: self.frame.size.height - width, width: self.frame.size.width, height: width)
-        self.layer.addSublayer(border)
-    }
-
-    func addLeftBorderWithColor(color: UIColor, width: CGFloat) {
-        let border = CALayer()
-        border.backgroundColor = color.cgColor
-        border.frame = CGRect(x: 0, y: 0, width: width, height: self.frame.size.height)
         self.layer.addSublayer(border)
     }
 }
